6efbedd364240265cef7229ffee1a82859c71b33
Vusalmb <vusalmb@code.edu.az>
2022-02-15 15:18:59 +0400

    Добавьте файлы проекта.

---
+++ ShopBooksApi/DAL/Data/ShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopBooksApi.DAL.Data
+{
+    public class ShopDbContext: DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options):base(options)
+        {
+        }
+
+
+    }
+}
