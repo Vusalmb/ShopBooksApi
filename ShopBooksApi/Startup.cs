180594c6559e9be2f6913e36e4d5e7a5d1cde7e0
Vusalmb <53233126+Vusalmb@users.noreply.github.com>
2022-02-18 16:27:10 +0400

    add services AutoMapper

--- ShopBooksApi/Startup.cs
+++ ShopBooksApi/Startup.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ShopBooksApi.Apps.AdminApi.DTOs.BookDTOs;
+using ShopBooksApi.Apps.AdminApi.Profiles;
 using ShopBooksApi.DAL.Data;
 using System;
 using System.Collections.Generic;
@@ -27,10 +30,16 @@ namespace ShopBooksApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(x=>x.RegisterValidatorsFromAssemblyContaining<BookPostDTO>());
             services.AddDbContext<ShopDbContext>(option =>
             {
                 option.UseSqlServer(Configuration.GetConnectionString("Default"));
+            });
+            services.AddAutoMapper(option =>
+            {
+                option.AddProfile(new AuthorMapProfile());
+                option.AddProfile(new GenreMapProfile());
+                option.AddProfile(new BookMapProfile());
             });
         }
 
