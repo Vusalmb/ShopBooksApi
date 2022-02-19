78d9fc0986017f31ec17c598cdc37f9897b4354e
Vusalmb <53233126+Vusalmb@users.noreply.github.com>
2022-02-19 23:56:35 +0400

    created DTOs

---
+++ ShopBookMVC/DTOs/CategoryListItemDTO.cs
@@ -0,0 +1,23 @@
+﻿using ShopBooksApi.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopBookMVC.DTOs
+{
+    public class CategoryListItemDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+        public string Detail { get; set; }
+        public string Language { get; set; }
+        public string Publishing { get; set; }
+        public string Cover { get; set; }
+        public string Weight { get; set; }
+        public Author Author { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
